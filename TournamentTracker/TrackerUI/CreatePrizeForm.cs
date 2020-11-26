2a9f4334eac0a671aa4523531bb6ae4c03365733
Jonathan <jonrhoads22@gmail.com>
2020-11-26 14:20:26 -0800

    Connected to SQL Server using Dapper

--- TournamentTracker/TrackerUI/CreatePrizeForm.cs
+++ TournamentTracker/TrackerUI/CreatePrizeForm.cs
@@ -65,10 +65,9 @@ namespace TrackerUI
                     prizeAmountValue.Text, 
                     prizePercentageValue.Text);
 
-                foreach (IDataConnection db in GlobalConfig.Connections)
-                {
-                    db.CreatePrize(model);
-                }
+                
+                GlobalConfig.Connection.CreatePrize(model);
+                
 
                 placeNameValue.Text = "";
                 placeNumberValue.Text = "";
