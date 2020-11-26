732de9d7d3b7b3d53e81463359651e036ad11735
Jonathan <jonrhoads22@gmail.com>
2020-11-26 12:11:09 -0800

    Adding DataAccess folder.

--- TournamentTracker/TrackerLibrary/Models/TeamModel.cs
+++ TournamentTracker/TrackerLibrary/Models/TeamModel.cs
@@ -5,7 +5,14 @@ namespace TrackerLibrary.Models
 {
     public class TeamModel
     {
+        /// <summary>
+        /// The list of members on a team.
+        /// </summary>
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+
+        /// <summary>
+        /// The name of the team.
+        /// </summary>
         public string TeamName { get; set; }
 
         
